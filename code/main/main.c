0bb685e0e82b809206d1b74a66bc6b286825f74f
PB-3117 <zacse38@gmail.com>
2024-06-24 23:49:10 -0400

    tests

--- code/main/main.c
+++ code/main/main.c
@@ -197,9 +197,6 @@ void app_main(void)
 
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
 
-    tinyusb_config_cdcacm_t acm_cfg = {0}; // the configuration uses default values
-    ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
-
     ESP_LOGI(TAG, "USB HID initialization DONE");
 
     tinyusb_config_cdcacm_t acm_cfg = {
@@ -208,7 +205,7 @@ void app_main(void)
         .rx_unread_buf_sz = 64,
         .callback_rx = NULL, // the first way to register a callback
         //.callback_rx = &tinyusb_cdc_rx_callback, // the first way to register a callback
-        .callback_rx_wanted_char = NULL;
+        .callback_rx_wanted_char = NULL,
         .callback_line_state_changed = NULL,
         .callback_line_coding_changed = NULL
     };
@@ -226,6 +223,8 @@ void app_main(void)
     vTaskDelay(100 / portTICK_PERIOD_MS);
     fprintf(stderr, "example: print -> stderr\n");
     vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    printf("hollo");
 
     ESP_LOGI(TAG, "console switched");
 
